8aa67777ff0b4df47bac79dc90cd276e90ef2be3
Jacob Good <jacobgood97@gmail.com>
2018-05-10 01:45:48 -0400

    first commit

---
+++ mtisdec5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace mtisdec5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			recfindfiles("/users/jacobgood/codeProjects/csharp");
+        }
+
+		static void recfindfiles(string path) 
+		{
+			DirectoryInfo d = new DirectoryInfo(path);
+			if (!(d.GetDirectories().Length == 0)) {
+				foreach (DirectoryInfo directory in d.GetDirectories()) {
+                    // Requires full path name to understand it's location
+					recfindfiles(directory.FullName);
+				}
+			}
+			foreach (FileInfo file in d.GetFiles()) {
+				Console.WriteLine(file.Name);
+			}
+		}
+    }
+}
